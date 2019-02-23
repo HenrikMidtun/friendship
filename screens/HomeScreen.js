c21be9b1fc3f5cf4ba31e4e3f0aaa9f5fe801bc1
HenrikMidtun <30526529+HenrikMidtun@users.noreply.github.com>
2019-02-23 15:50:48 +0100

    added Timer and Date to TicketScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -20,26 +20,29 @@ export default class HomeScreen extends React.Component {
   {
     super(props);
     this.state={count:0,};
+    //this.goToScreen = this.goToScreen.bind(this);
   }
-
 
   render() {
     const {navigate} = this.props.navigation;
     return (
       <ScrollView style={styles.container}>
         <Text>Hello World!</Text>
-        <Button title={String(this.state.count)} onPress={()=> navigate('Ticket',{count:this.state.count})}/>
+        <Button title={String(this.state.count)} onPress={this._goToScreen}/>
         <Button title='more' onPress={this._incrementCount}/>
         <Button title='less' onPress={this._decrementCount}/>
       </ScrollView>
     );
   }
 
-
+  _goToScreen = () => {
+    this.props.navigation.navigate('Ticket',{count:this.state.count})
+  };
 
   _incrementCount = () => {this.setState({count: this.state.count + 1})};
  
   _decrementCount = () => {this.setState({count: this.state.count - 1})};
+
 
 }
 
--- screens/TicketScreen.js
+++ screens/TicketScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ScrollView, View, StyleSheet, Text, ImageBackground, Image, Dimensions } from 'react-native';
+import PropTypes from 'prop-types';
 import { ExpoLinksView } from '@expo/samples';
 
 export default class TicketScreen extends React.Component {
@@ -7,20 +8,42 @@ export default class TicketScreen extends React.Component {
   constructor(props)
   {
     super(props);
+    this.state = {timer:15, date:new Date()};
   }
   
   render() {
     const {navigate} = this.props.navigation;
     return (
-
-      <ImageBackground source={require('../assets/images/banana.png')} style={{width:Dimensions.get('window').width, heigth:Dimensions.get('window').heigth}}>
-        <View style={styles.container}>
-          <Text>{String(this.props.navigation.state.params.count)}</Text>
-        </View>
-      </ImageBackground>
+      <View style={styles.container}>
+        <Text>{String(this.props.navigation.state.params.count)}</Text>
+        <Text>{String(this.state.timer)}</Text>
+        <Text>Day: {String(this.state.date.getDate())}</Text>
+        <Text>Hour: {String(this.state.date.getHours())}</Text>
+      </View>
     );
   }
+
+
+  componentDidMount()
+  {
+    this.interval = setInterval(
+      () => this.setState((prevState) => ({timer: prevState.timer -1 })),1000);
+  }
+
+  componentDidUpdate()
+  {
+    if(this.state.timer === 1)
+    {
+      clearInterval(this.interval);
+    }
+  }
+  componentWillUnmount()
+  {
+    clearInterval(this.interval);
+  }
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
